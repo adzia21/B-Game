cd41fc2e35f3eabf61ecc9ecb69f501e86399d69
Angelika <angelika.swiackaa@gmail.com>
2022-10-08 15:02:15 +0200

    Init

---
+++ src/main/java/com/example/bgame/BGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.bgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bgame/BGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
